283baed6803464d977d25fd50b7cac7656f8d1f1
Dart <Danslis@yandex.ru>
2023-06-15 18:19:14 +0300

    работа с отзывами, добавление отзыва к отзыву

--- movies/views.py
+++ movies/views.py
@@ -46,12 +46,13 @@ class MovieDetailView(DetailView):
 
 class AddReview(View):
     """Отзывы"""
-
     def post(self, request, pk):
         form = ReviewForm(request.POST)
         movie = Movie.objects.get(id=pk)
         if form.is_valid():
             form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
